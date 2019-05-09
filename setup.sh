2d52ada521aa11beea1d1d3c37692cd732856f4e
Luciano Barreira <luc.s.barreira@gmail.com>
2019-05-09 10:17:00 -0300

    Add speech to text example

--- setup.sh
+++ setup.sh
@@ -1,10 +1,2 @@
 JSONFILE=$(ls | grep .json)
 export GOOGLE_APPLICATION_CREDENTIALS="$GOPATH/src/github.com/matheusbortolini/babel-io/$JSONFILE"
-
-CMD="dep version"
-if $CMD | grep -q 'platform'; then
-     echo "dep already installed"
- else
-  set -x
-   $(go get -u github.com/golang/dep)
-fi
